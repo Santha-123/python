fabc42584fff6e88841771eaf697f13d2491df5d
Santha-123 <141953836+Santha-123@users.noreply.github.com>
2024-07-12 12:40:55 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,19 @@
+import math
+def circle_radius():
+    '''
+    To find the area of the circle
+    Circle(radius
+    radius-enter the radius of the circle)
+    '''
+    radius=5.0
+    area=math.pi* radius **2
+    return area
+def rectangle(length,breadth):
+    '''
+    To find the area of the rectangle
+    rectangle(length,breadth)
+    length=enter the length of the recatngle
+    breadth=enter the breadth of the recatngle
+    '''
+    area=length*breadth
+    return area
---
+++ areas.py
@@ -0,0 +1,10 @@
+import math
+def cir_area(r):
+    a=math.pi*r*r
+    return a
+def squ_area(a):
+    return a*a
+def rect_area(l,b):
+    return l*b
+def tri_area(l,b):
+    return (l*b)/2
